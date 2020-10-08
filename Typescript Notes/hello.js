dabd569a0868c112cf956348f9143c29706b577c
Nandha <nanda.postbox@gmail.com>
2020-10-08 20:10:56 +0530

    notes

---
+++ Typescript Notes/hello.js
@@ -0,0 +1,32 @@
+ 
+ 
+ function getName() {
+  if(true){
+    let grade = "a";
+    console.log(grade)
+  }
+  
+}
+
+getName();
+
+
+
+
+
+ function updateName(){
+var name ="Nanda"
+   console.log("hi",name);
+ }
+ console.log("hi",name);
+ updateName();
+
+ 
+
+
+ 
+
+ 
+ 
+ 
+
